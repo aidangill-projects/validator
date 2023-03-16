6a64c6b0d0711318ad483d8ed655e1458460ca32
Aidan <aidan.gill@oncallhealth.co.nz>
2023-03-16 17:01:29 +1300

    adjusted to utilise the validator interface rather than the method

--- Validator/Entry.cs
+++ Validator/Entry.cs
@@ -4,7 +4,10 @@ internal static class Entry
 {
     private static void Main(string[] args)
     {
-        DefaultValidator v = new DefaultValidator();
-        v.IsValid("1234");
+        IValidator v1 = new DefaultValidator();
+        v1.IsValid("1234");
+        
+        IValidator v2 = new AlwaysTrueValidator();
+        v2.IsValid("1234");
     }
 }
